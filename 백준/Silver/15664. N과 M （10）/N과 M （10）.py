d5a749714cafc22ff03124ee0449592d0e03dc1e
NEU <96049463+NEU-chaldea@users.noreply.github.com>
2023-02-16 01:35:41 +0900

    [Silver II] Title: N과 M (10), Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -0,0 +1,24 @@
+
+N, M = map(int, input().split())
+save = list(map(int, input().split()))
+save.sort()
+lst = []
+cnt = 0
+visited = [False] * (N+1)
+
+def DFS(depth, k):
+    prev = 0
+    if depth == M:
+        print(*lst)
+        return
+    else:
+        for i in range(k, N):
+            if save[i] != prev and visited[i] == False:
+                lst.append(save[i])
+                prev = save[i]
+                visited[i] = True
+                DFS(depth + 1, i)
+                lst.pop()
+                visited[i] = False
+
+DFS(0, 0)
