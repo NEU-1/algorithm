21f8812e3d581184c4955c75857f2216de92cd6d
NEU <96049463+NEU-chaldea@users.noreply.github.com>
2023-02-15 23:51:27 +0900

    [Silver III] Title: N과 M (6), Time: 72 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -0,0 +1,11 @@
+def com(num, start, M):
+    if M == 0:
+        print(*num)
+    else:
+        for i in range(start, N):
+            com(num + [save[i]], i+1, M-1)
+
+N, M = map(int, input().split())
+save = list(map(int, input().split()))
+save.sort()
+com([], 0, M)
