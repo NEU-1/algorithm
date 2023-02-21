804eb610560c134a0aabd498e6631ec86e7bf231
NEU <96049463+NEU-chaldea@users.noreply.github.com>
2023-02-21 17:05:43 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 138 ms, Memory: 52,892 KB -BaekjoonHub

---
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+for t in range(1,11):
+    _ = input()
+    maze = [list(str(input())) for _ in range(16)]
+    runs = deque([(1,1)])
+    # print(maze)
+
+    y, x = 1, 1
+    dy = [1,0,-1,0]
+    dx = [0,1,0,-1]
+    fin = 0
+
+    while runs:
+        # print(runs)
+        now = runs.popleft()
+        y, x = now[0], now[1]
+        # print(y, x)
+        for i in range(4):
+            ny = y+dy[i]
+            nx = x+dx[i]
+            # print(ny, nx)
+            if ny < 0 or nx < 0 or ny >= 16 or nx >= 16:
+                continue
+            elif maze[ny][nx] == '1':
+                continue
+            elif maze[ny][nx] == '3':
+                fin = 1
+                break
+            elif maze[ny][nx] == '0':
+                runs.append((ny,nx))
+                # print(runs)
+                maze[ny][nx] = 1
+    
+    print(f'#{t} {fin}')
+
+
+    
