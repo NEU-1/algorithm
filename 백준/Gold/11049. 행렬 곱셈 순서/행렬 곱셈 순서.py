bb79e71d9f10a6766eea89b4bb904ec7dc10311a
NEU <96049463+NEU-chaldea@users.noreply.github.com>
2023-04-25 22:10:28 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 880 ms, Memory: 118452 KB -BaekjoonHub

---
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+matrix = [tuple(map(int, input().split())) for _ in range(n)]
+
+dp = [[0] * n for _ in range(n)]
+
+for i in range(1, n):
+    for j in range(n - i):
+        x = j + i
+        dp[j][x] = float('inf')
+        for k in range(j, x):
+            dp[j][x] = min(dp[j][x], dp[j][k] + dp[k + 1][x] + matrix[j][0] * matrix[k][1] * matrix[x][1])
+
+print(dp[0][-1])
